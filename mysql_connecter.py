6d0dd9ae736de3f24672751c3e36d11369662013
Keyog <jgy206@gmail.com>
2021-05-06 09:17:38 +0000

    kaka - mysql 연동 작업 완료

--- mysql_connecter.py
+++ mysql_connecter.py
@@ -19,6 +19,21 @@ class MySQL :
         if not MYSQL_CONN.open :
             MYSQL_CONN.ping(reconnect=True)
         return MYSQL_CONN
+
+    def create_sql_item(self,item,dtype) :
+        if dtype == 'string' or 'datetime':
+            return "'{}'".format(item)
+        elif dtype == 'bool' or 'int':
+            return "{}".format(item)
+
+    def dict_list2string(self,items,dtype_map) :
+        key_arr = []
+        item_arr = []
+        for key, item in items.items() :
+            if key != 'id' and item != None :
+                key_arr.append(key)
+                item_arr.append(self.create_sql_item(item,dtype=dtype_map[key]))
+        return arr2str(key_arr),arr2str(item_arr)
     
     def insert_data(self,table,items,dtype_map) :
         skey,sdata = self.dict_list2string(items,dtype_map)
--- run.py
+++ run.py
@@ -1,5 +1,39 @@
 from mysql_connecter import MySQL
+from kafka import KafkaConsumer 
+from json import loads 
+# topic, broker list 
+consumer = KafkaConsumer('flask_all_logs', 
+                         bootstrap_servers=['52.78.62.228:9092'],
+                         auto_offset_reset='earliest',
+                         enable_auto_commit=True,
+                         group_id='my-group',
+                         value_deserializer=lambda x: loads(x.decode('utf-8')),
+                         consumer_timeout_ms=1000 ) 
 
 mysql = MySQL(key_file='./keys/aws_dc_sql_key.json',
               database='career-center')
+with open('./keys/dtype_map.json') as file :
+    dtype_map = json.load(file)
 
+for message in consumer :
+    if message.key == 'analys' :
+        data = message.value['Message']
+        datetime = message['Asctime']
+        table_name = data['activity']
+        sql_dict = {
+                "user_id" : data['user_id'],
+                "datetime" : datetime
+            }
+        if table_name == 'resume_sector' :
+            sql_dict['sector_id'] = data['resume_select']
+        elif table_name == 'resume_skill' :
+            sql_dict['skill_id'] = data['resume_select']
+        elif table_name == 'filtering' :
+            sql_dict['filtering'] = data['filter_text']
+        else :
+            sql_dict['recruit_id'] = data['recruit_id']
+
+        mysql.insert_data(table_name,sql_dict,dtype_map)
+        print(f"[SQL INPUT]Time : {datetime}, Key: {message.key}, Value: {data}")
+
+        
